86dd065cfff19d2b5da6dc558588705a9b666acc
Steven Cooney <30004860+TheYorkshireDev@users.noreply.github.com>
2022-10-28 18:38:06 +0100

    Add Failing Test

--- test/examples_basic_github_repository_test.go
+++ test/examples_basic_github_repository_test.go
@@ -3,15 +3,22 @@ package test
 import (
 	"testing"
 
+	"github.com/jaswdr/faker"
+
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestExamplesBasicGithubRepository(t *testing.T) {
-	expectedOutput := "initial_output"
+	faker := faker.New()
+
+	repositoryName := faker.RandomStringWithLength(8)
 
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "../examples/basic-github-repository",
+		Vars: map[string]interface{}{
+			"name": repositoryName,
+		},
 	})
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
@@ -19,8 +26,8 @@ func TestExamplesBasicGithubRepository(t *testing.T) {
 
 	// Apply the infrastructure
 	terraform.InitAndApply(t, terraformOptions)
-	output := terraform.Output(t, terraformOptions, "initial_output")
-	assert.Equal(t, expectedOutput, output)
+	actualRepositoryName := terraform.Output(t, terraformOptions, "repository_name")
+	assert.Equal(t, repositoryName, actualRepositoryName)
 
 	// Run perpetual diff
 	planResult := terraform.Plan(t, terraformOptions)
