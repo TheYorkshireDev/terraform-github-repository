c467b756c9bf7fefe62af8203d163cc80ed580bb
Steven Cooney <30004860+TheYorkshireDev@users.noreply.github.com>
2022-11-07 22:07:49 +0000

    a

--- aaa.js
+++ aaa.js
@@ -1,7 +1,18 @@
+const path = require('node:path');
+const glob = require('glob');
 const execSync = require('child_process').execSync;
-// import { execSync } from 'child_process';  // replace ^ if using ES modules
+
+var directories = []
+
+let files = []
+files=glob.sync("**/*.tf", {})
+console.log(files)
+files.forEach(file => {
+  var directory = path.dirname(file)
+  if (directories.indexOf(directory) === -1)
+    directories.push(directory)
+});
+console.log(directories.sort())
 
 const output = execSync('terraform-docs markdown table --indent 2 .', { encoding: 'utf-8' });  // the default is 'buffer'
-
-
 console.log(output);
