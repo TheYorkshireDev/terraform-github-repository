8095b9ce8a69f98c0e99c2d26296cb606cdec79d
Steven Cooney <30004860+TheYorkshireDev@users.noreply.github.com>
2022-11-08 20:44:27 +0000

    a

--- .github/scripts/commenter.js
+++ .github/scripts/commenter.js
@@ -1,3 +1,22 @@
 module.exports = ({github, context}) => {
-  return context.payload.client_payload.value
+  const path = require('node:path');
+  const globSearch = require('glob');
+
+  function findTerraformDirectories() {
+    var directories = []
+  
+    let files = []
+    files = globSearch.sync("**/*.tf", {})
+    files.forEach(file => {
+      var directory = path.dirname(file)
+      if (directories.indexOf(directory) === -1)
+        directories.push(directory)
+    });
+  
+    return directories.sort()
+  }
+
+  // Run:
+  var directories = findTerraformDirectories();
+  console.log(directories)
 }
